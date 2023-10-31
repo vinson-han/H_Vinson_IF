456956c7b9980a832e581db71b1f00834711ae98
vinson-han <han_vinson@student.smc.edu>
2023-10-30 22:30:47 -0700

    done

---
+++ Week5/week_5_dom_exercise_3/script.js
@@ -0,0 +1,15 @@
+
+//
+document.getElementById('example1').style.background = "yellow"
+
+document.getElementById('example2').style.fontSize = "24px"
+
+document.getElementsByClassName('example3')[0].style.color = 'red'
+
+document.getElementById('example4').style.border = "1px solid black"
+
+document.getElementById('example5').style.backgroundImage = "url('https://images.pexels.com/photos/255379/pexels-photo-255379.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')"
+
+document.getElementById('example6').style.visibility = "hidden"
+
+ let x = document.getElementsByClassName('example7')[0].style.color = "green"
