face7d3c7a61489be26164d408466982424cc0d0
vinson-han <han_vinson@student.smc.edu>
2023-10-30 18:12:49 -0700

    w5e1

---
+++ Week5/week_5_dom_exercise_1/script.js
@@ -0,0 +1,8 @@
+let heading = document.querySelector("h1")
+console.log(heading.innerText)
+
+let p = document.querySelectorAll('p')
+console.log(p[1].textContent)
+
+let li = document.querySelectorAll('li')
+console.log(li[2].innerText)
