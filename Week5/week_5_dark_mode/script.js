af82ee9ba09e0ddf09e254e40a81d5b8e3b93bab
vinson-han <han_vinson@student.smc.edu>
2023-10-31 18:30:24 -0700

    w5e2

---
+++ Week5/week_5_dark_mode/script.js
@@ -0,0 +1,21 @@
+const toggleMode = () => {
+
+	let button = document.querySelector('button')
+	let body = document.querySelector('body')
+
+	if(button.textContent === "dark mode"){
+		body.style.background = "#0A0A0A"
+		body.style.color = 'white'
+		button.textContent = "light mode"
+	}else {
+		body.style.background = "white"
+		body.style.color = 'black'
+		button.textContent = "dark mode"
+	}
+
+
+
+	
+
+
+}
