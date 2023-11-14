cf359085726c2e743682c59de8a0ad451c44ee5e
vinson-han <han_vinson@student.smc.edu>
2023-11-13 18:57:53 -0800

    w7

---
+++ Week7/week_7_create_student_class_exercise_2/index.js
@@ -0,0 +1,11 @@
+class Student {
+	constructor(name, age, major) {
+		this.name = name;
+		this.age= age;
+		this.major= major;
+	}
+}
+
+const Bob = new Student("Bob", 23, "CS")
+
+console.log(Bob)
