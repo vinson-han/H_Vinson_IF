fd1d8332bae957ca9c4b190c31c399d09ce9436d
vinson-han <han_vinson@student.smc.edu>
2023-11-20 18:29:02 -0800

    v2

--- Week8/week_8_json_exercise/json_exercise_2.js
+++ Week8/week_8_json_exercise/json_exercise_2.js
@@ -1,23 +1,10 @@
-const dataObject = {
-  user: {
-    id: 1,
-    name: "John Doe",
-    email: "john@example.com",
+const fs = require("fs");
+fs.readFile("./dummyData.json", "utf8", (err, jsonString) => {
+  if (err) {
+    console.log("File read failed:", err);
+    return;
+  }
 
-    address: {
-      city: "New York",
-
-      zipcode: "10001",
-    },
-  },
-
-  product: {
-    id: 101,
-
-    name: "Laptop",
-
-    price: 999.99,
-  },
-};
-
-console.log(JSON.stringify(dataObject));
+  let dataObject = JSON.parse(jsonString);
+  console.log(JSON.stringify(dataObject));
+});
